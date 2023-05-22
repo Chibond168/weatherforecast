db7f76957bcc4a2bb36a147479bbcdfbeac4df75
Chi Leung <chileung@Chis-iMac.local>
2023-05-22 14:28:39 -0600

    Working on CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // save reference to important DOM elements
-var buttonGoSearch = $('#go-search');
+var buttonGoSearch = document.querySelector('#go-search');
 var apikey = '&APPID=619585d8879c30ef87ab6a89c84609be';
 //var requesturl = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}";
 var requesturl = "https://api.openweathermap.org/data/2.5/forecast?";
 var requestloc = "https://api.openweathermap.org/geo/1.0/direct?q="; 
 //var findlocation = "https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
 
-function handleCFindCityLat(event) {
+var handleCFindCityLat = function (event) {
 
-  goFindMyStuff(39.7392364, -104.984862);
+    goFindMyStuff(39.7392364, -104.984862);
     var parseParm = $('#search-city').val()
     parseParm = parseParm.trim();
     if (parseParm.length == 0)
@@ -33,33 +33,31 @@ function handleCFindCityLat(event) {
         alert('Unable to connect to GitHub');
       });
 
-     
-      //goFindMyStuff(39.7392364, -104.984862);
     }
+}
+
+var retrieveLatRepos = function (repos) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No repositories found.';
+    return;
   }
 
-  var retrieveLatRepos = function (repos) {
-    if (repos.length === 0) {
-      repoContainerEl.textContent = 'No repositories found.';
-      return;
-    }
-  
-    var reposLat = repos[0].lat;
-    var reposLon = repos[0].lon;
+  var reposLat = repos[0].lat;
+  var reposLon = repos[0].lon;
 
-    goFindMyStuff(reposLat, reposLon)
+  goFindMyStuff(reposLat, reposLon)
 
-  }
+}
 
-function goFindMyStuff(parlat, parlon){
+var goFindMyStuff = function(parlat, parlon){
 
-    var findweather = requesturl + "lat=" + parlat + "&lon=" + parlon + apikey;
+    var findweather = requesturl + "lat=" + parlat + "&lon=" + parlon + apikey + "&units=imperial";
 
     fetch(findweather)
     .then(function (weatherresponse) {
       if (weatherresponse.ok) {
         weatherresponse.json().then(function (weatherdata) {
-          rendorWeatherRepos(weatherdata);
+          rendorWeatherRepos(weatherdata.list);
         });
       } else {
         alert('Error: ' + weatherresponse.statusText);
@@ -70,35 +68,44 @@ function goFindMyStuff(parlat, parlon){
     });
 }
 
-function rendorWeatherRepos(weatherrepos)
+var rendorWeatherRepos = function(weatherrepos)
 {
+  if (weatherrepos.length === 0) {
+    //repoContainerEl.textContent = 'No repositories found.';
+    return;
+  }
+
   for (var i = 0; i < weatherrepos.length; i++) {
-    var repoName = weatherrepos[i].list.login + '/' + weatherrepos[i].name;
+    
+    var reposDate = weatherrepos[i].dt_txt;
+    var reposHumidity = weatherrepos[i].main.humidity;
+    var reposWind = weatherrepos[i].wind.speed;
+    var reposWeather = weatherrepos[i].weather[0].main;
+    var reposTemp = weatherrepos[i].main.temp;
 
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    //var repoEl = document.createElement('div');
+    //repoEl.classList = 'list-item flex-row justify-space-between align-center';
 
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
+    //var titleEl = document.createElement('span');
+    //titleEl.textContent = repoName;
 
-    repoEl.appendChild(titleEl);
+    //repoEl.appendChild(titleEl);
 
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
+    //var statusEl = document.createElement('span');
+    //statusEl.classList = 'flex-row align-center';
 
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
+    //if (repos[i].open_issues_count > 0) {
+    //  statusEl.innerHTML =
+    //    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+    //} else {
+    //  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    //}
 
-    repoEl.appendChild(statusEl);
+    //repoEl.appendChild(statusEl);
 
-    repoContainerEl.appendChild(repoEl);
+    //repoContainerEl.appendChild(repoEl);
   
   }
 }
 
-
-buttonGoSearch.on('click', handleCFindCityLat);
+buttonGoSearch.addEventListener('click', handleCFindCityLat);

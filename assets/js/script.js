dd2222a0b017a1177c5d7e5650a1b579107f9986
Chi Leung <chileung@Chis-iMac.local>
2023-05-23 22:49:12 -0600

    Fixing style

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,37 @@ var apikey = '&APPID=619585d8879c30ef87ab6a89c84609be';
 var requesturl = "https://api.openweathermap.org/data/2.5/forecast?";
 var requestloc = "https://api.openweathermap.org/geo/1.0/direct?q="; 
 //var findlocation = "https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
+
+var weathericon = "https://openweathermap.org/img/wn/???@2x.png"
 var currentcity = document.querySelector("#current-city");
 var currenttemp = document.querySelector("#current-temp");
 var currentwind = document.querySelector("#current-wind");
+var currentweathericon = document.querySelector("#current-weather-icon");
 var currenthumidity = document.querySelector("#current-humidity");
 var day1date = document.querySelector("#day1-date");
 var day1temp = document.querySelector("#day1-temp");
 var day1wind = document.querySelector("#day1-wind");
+var day1weathericon = document.querySelector("#day1-weather-icon");
 var day1humidity = document.querySelector("#day1-humidity");
 var day2date = document.querySelector("#day2-date");
 var day2temp = document.querySelector("#day2-temp");
 var day2wind = document.querySelector("#day2-wind");
+var day2weathericon = document.querySelector("#day2-weather-icon");
 var day2humidity = document.querySelector("#day2-humidity");
 var day3date = document.querySelector("#day3-date");
 var day3temp = document.querySelector("#day3-temp");
 var day3wind = document.querySelector("#day3-wind");
+var day3weathericon = document.querySelector("#day3-weather-icon");
 var day3humidity = document.querySelector("#day3-humidity");
 var day4date = document.querySelector("#day4-date");
 var day4temp = document.querySelector("#day4-temp");
 var day4wind = document.querySelector("#day4-wind");
+var day4weathericon = document.querySelector("#day4-weather-icon");
 var day4humidity = document.querySelector("#day4-humidity");
 var day5date = document.querySelector("#day5-date");
 var day5temp = document.querySelector("#day5-temp");
 var day5wind = document.querySelector("#day5-wind");
+var day5weathericon = document.querySelector("#day5-weather-icon");
 var day5humidity = document.querySelector("#day5-humidity");
 
 var handleCFindCityLat = function (event) {
@@ -82,6 +90,7 @@ var goFindMyStuff = function(parlat, parlon){
     .then(function (weatherresponse) {
       if (weatherresponse.ok) {
         weatherresponse.json().then(function (weatherdata) {
+          currentcity.innerHTML = weatherdata.city.name;
           rendorWeatherRepos(weatherdata.list);
         });
       } else {
@@ -100,71 +109,91 @@ var rendorWeatherRepos = function(weatherrepos)
       return;
     }
  
-    var reposDate = weatherrepos[0].dt_txt;
+    var reformatDate = weatherrepos[0].dt_txt;
+    var reposDate = dayjs(reformatDate).format('MM/DD/YYYY');
+
     var reposHumidity = weatherrepos[0].main.humidity;
     var reposWind = weatherrepos[0].wind.speed;
-    var reposWeather = weatherrepos[0].weather[0].main;
+    var reposWeather = weatherrepos[0].weather[0].icon;
     var reposTemp = weatherrepos[0].main.temp;
 
-    currentcity.innerHTML = searchcity.value + " " + reposDate;
+    var imgicon = weathericon.replace("???", reposWeather)
+
+    currentcity.innerHTML = currentcity.innerHTML + " " + reposDate;
     currenttemp.innerHTML = reposTemp;
     currentwind.innerHTML = reposWind;
     currenthumidity.innerHTML = reposHumidity;
+    currentweathericon.src = imgicon;
     
-    reposDate = weatherrepos[1].dt_txt;
-    reposHumidity = weatherrepos[1].main.humidity;
-    reposWind = weatherrepos[1].wind.speed;
-    reposWeather = weatherrepos[1].weather[0].main;
-    reposTemp = weatherrepos[1].main.temp;
+    reformatDate = weatherrepos[6].dt_txt;
+    reposDate = dayjs(reformatDate).format('MM/DD/YYYY');
+    reposHumidity = weatherrepos[6].main.humidity;
+    reposWind = weatherrepos[6].wind.speed;
+    reposWeather = weatherrepos[6].weather[0].icon;
+    reposTemp = weatherrepos[6].main.temp;
+    imgicon = weathericon.replace("???", reposWeather)
 
     day1date.innerHTML = reposDate;
     day1temp.innerHTML = reposTemp;
     day1wind.innerHTML = reposWind;
     day1humidity.innerHTML = reposHumidity;
+    day1weathericon.src = imgicon;
     
-    reposDate = weatherrepos[2].dt_txt;
-    reposHumidity = weatherrepos[2].main.humidity;
-    reposWind = weatherrepos[2].wind.speed;
-    reposWeather = weatherrepos[2].weather[0].main;
-    reposTemp = weatherrepos[2].main.temp;
+    reformatDate = weatherrepos[14].dt_txt;
+    reposDate = dayjs(reformatDate).format('MM/DD/YYYY');
+    reposHumidity = weatherrepos[14].main.humidity;
+    reposWind = weatherrepos[14].wind.speed;
+    reposWeather = weatherrepos[14].weather[0].icon;
+    reposTemp = weatherrepos[14].main.temp;
+    imgicon = weathericon.replace("???", reposWeather)
 
     day2date.innerHTML = reposDate;
     day2temp.innerHTML = reposTemp;
     day2wind.innerHTML = reposWind;
     day2humidity.innerHTML = reposHumidity;
+    day2weathericon.src = imgicon;
     
-    eposDate = weatherrepos[3].dt_txt;
-    reposHumidity = weatherrepos[3].main.humidity;
-    reposWind = weatherrepos[3].wind.speed;
-    reposWeather = weatherrepos[3].weather[0].main;
-    reposTemp = weatherrepos[3].main.temp;
+    reformatDate = weatherrepos[22].dt_txt;
+    reposDate = dayjs(reformatDate).format('MM/DD/YYYY');
+    reposHumidity = weatherrepos[22].main.humidity;
+    reposWind = weatherrepos[22].wind.speed;
+    reposWeather = weatherrepos[22].weather[0].icon;
+    reposTemp = weatherrepos[22].main.temp;
+    imgicon = weathericon.replace("???", reposWeather)
 
     day3date.innerHTML = reposDate;
     day3temp.innerHTML = reposTemp;
     day3wind.innerHTML = reposWind;
     day3humidity.innerHTML = reposHumidity;
+    day3weathericon.src = imgicon;
     
-    reposDate = weatherrepos[4].dt_txt;
-    reposHumidity = weatherrepos[4].main.humidity;
-    reposWind = weatherrepos[4].wind.speed;
-    reposWeather = weatherrepos[4].weather[0].main;
-    reposTemp = weatherrepos[4].main.temp;
+    reformatDate = weatherrepos[30].dt_txt;
+    reposDate = dayjs(reformatDate).format('MM/DD/YYYY');
+    reposHumidity = weatherrepos[30].main.humidity;
+    reposWind = weatherrepos[30].wind.speed;
+    reposWeather = weatherrepos[30].weather[0].icon;
+    reposTemp = weatherrepos[30].main.temp;
+    imgicon = weathericon.replace("???", reposWeather)
 
     day4date.innerHTML = reposDate;
     day4temp.innerHTML = reposTemp;
     day4wind.innerHTML = reposWind;
     day4humidity.innerHTML = reposHumidity;
+    day4weathericon.src = imgicon;
     
-    reposDate = weatherrepos[5].dt_txt;
-    reposHumidity = weatherrepos[5].main.humidity;
-    reposWind = weatherrepos[5].wind.speed;
-    reposWeather = weatherrepos[5].weather[0].main;
-    reposTemp = weatherrepos[5].main.temp;
+    reformatDate = weatherrepos[38].dt_txt;
+    reposDate = dayjs(reformatDate).format('MM/DD/YYYY');
+    reposHumidity = weatherrepos[38].main.humidity;
+    reposWind = weatherrepos[38].wind.speed;
+    reposWeather = weatherrepos[38].weather[0].icon;
+    reposTemp = weatherrepos[38].main.temp;
+    imgicon = weathericon.replace("???", reposWeather)
 
     day5date.innerHTML = reposDate;
     day5temp.innerHTML = reposTemp;
     day5wind.innerHTML = reposWind;
     day5humidity.innerHTML = reposHumidity;
+    day5weathericon.src = imgicon;
   
 }
 
